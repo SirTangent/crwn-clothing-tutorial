47f87e2270c67564666f28e0e54e2b3d2b5f8d25
Wyatt Phillips <phillipsw1@wit.edu>
2020-04-13 00:15:07 -0400

    Add Router DOM

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
+
 import './App.css';
 
 import HomePage from "./pages/homepage";
 
+const HatsPage = () => (
+    <div>HATS PAGE</div>
+)
+
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
-    </div>
+      <div>
+          <Switch>
+              <Route exact path='/' component={HomePage}/>
+              <Route path='/hats' component={HatsPage}/>
+          </Switch>
+      </div>
   );
 }
 
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Directory from "../components/directory/directory";
 import './homepage.scss';
 
-const HomePage = () => (
+const HomePage = ({history}) => (
     <div className="homepage">
         <Directory />
     </div>
